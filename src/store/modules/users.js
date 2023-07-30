b5f6bb709c2d93bbdd30731d9621f156242c451d
euclides <euclideskinto@gmail.com>
2023-07-30 20:17:48 -0300

    feat: add module users

---
+++ src/store/modules/users.js
@@ -0,0 +1,36 @@
+// store/modules/pessoas.js
+export default {
+    state: {
+        users: [],
+        nextUserId: 1
+    },
+    getters: {
+        getAllUsers: (state) => state.users,
+        getTotalUsers: (state) => state.users.length,
+    },
+    mutations: {
+        addUser(state, user) {
+            user.id = state.nextUserId;
+            state.users.push(user);
+            state.nextUserId++;
+            localStorage.setItem("users", JSON.stringify(state.users));
+        },
+        removeUser(state, user) {
+            const index = state.users.findIndex((u) => u.id === user.id);
+            if (index !== -1) {
+                state.users.splice(index, 1);
+                localStorage.setItem("users", JSON.stringify(state.users));
+            }
+        },
+        setUsers(state, users) {
+            state.users = users;
+            console.log(users)
+        },
+    },
+    actions: {
+        async initializeState({ commit }) {
+            const users = JSON.parse(localStorage.getItem("users")) || [];
+            commit("setUsers", users);
+        },
+    },
+};
